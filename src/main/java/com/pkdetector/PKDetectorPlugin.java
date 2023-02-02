36c7924e2109c8f24f21ee3d2e7143e98cba40f7
Proteus <proteus.osrs@gmail.com>
2023-02-02 11:42:28 -0500

    First commit of working project.

--- src/main/java/com/pkdetector/PKDetectorPlugin.java
+++ src/main/java/com/pkdetector/PKDetectorPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.pkdetector;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "PK Detector"
 )
-public class ExamplePlugin extends Plugin
+public class PKDetectorPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private PKDetectorConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	PKDetectorConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(PKDetectorConfig.class);
 	}
 }
--- src/test/java/com/pkdetector/PKDetectorPluginTest.java
+++ src/test/java/com/pkdetector/PKDetectorPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.pkdetector;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PKDetectorPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PKDetectorPlugin.class);
 		RuneLite.main(args);
 	}
 }
